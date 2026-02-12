004c61137f0a71a29b89e586339d317281299f36
Boram Kim <boram2445@gmail.com>
2026-02-12 23:45:47 +0900

    Chore: next 16 셋팅

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/jsx-key': 'warn',
+
+      'no-console': 'warn',
+    },
+  },
+]);
+
+export default eslintConfig;
